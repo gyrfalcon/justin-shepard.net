ec1533762b00a8547dbcb69b660414cbca7be410
Justin Shepard <justin.shepard@gmail.com>
2017-12-06 21:04:58 -0800

    Initial Commit
    Initial commit of basic files.

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ['standard', 'plugin:react/recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'flowtype',
+  ],
+  rules: {
+    indent: [ 'error', 2, {
+      SwitchCase: 1,
+    }],
+    'comma-dangle': [ 'error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'always-multiline',
+    }],
+    'flowtype/require-valid-file-annotation': 'error',
+    'flowtype/define-flow-type': 'error',
+    'max-len': ['error', 120, {
+      ignoreUrls: true,
+      ignoreStrings: true,
+      ignoreComments: true,
+      ignoreRegExpLiterals: true,
+      ignoreTemplateLiterals: true,
+    }],
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'prefer-arrow-callback': 'error',
+  }
+}
---
+++ main/src/components/App.jsx
@@ -0,0 +1,12 @@
+// @flow
+import React from 'react'
+
+type AppProps = {}
+
+export default function App (props: AppProps) {
+  return (
+    <div style={{textAlign: 'center'}}>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
