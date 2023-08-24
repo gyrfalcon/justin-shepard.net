1570fce40340f0ae135ff2022e8202f204372b42
Justin Shepard <justin.shepard@gmail.com>
2023-08-23 18:43:40 -0700

    Eject Babel
    Ejects babel, it won't be needed where we're going.

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,6 @@ module.exports = {
   mode: 'production',
   module: {
     rules: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
     ],
   },
   output: {
